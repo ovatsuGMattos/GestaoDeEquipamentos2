c16375160d773083b89f824fa24698a08461e936
ovatusGMattos <gustavo.j.mattos1@gmail.com>
2025-04-05 15:04:57 -0300

    Add project files.

---
+++ GestaoDeEquipamentos2.ConsoleApp/GeradordeId.cs
@@ -0,0 +1,20 @@
+﻿namespace GestaoDeEquipamentos.ConsoleApp;
+
+public static class GeradorIds
+{
+
+    //• Deve ter um identificador único(Id);
+    public static int IdChamados = 0;
+
+    public static int GerarIdChamados()
+    {
+        IdChamados++;
+
+        return IdChamados;
+    }
+
+    public static Guid GerarGuidChamado()
+    {
+        return Guid.NewGuid();
+    }
+}
---
+++ GestaoDeEquipamentos2.ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System.Runtime.Intrinsics.X86;
+using System;
+
+namespace GestaoDeEquipamentos2.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. Controle de Chamados
+
+            //Requisito 2.1: Como funcionário Junior quer ter a possibilidade de registrar os chamados de manutenções que são efetuadas nos equipamentos registrados
+            
+            //• Deve ter o título do chamado;
+            //• Deve ter a descrição do chamado;
+            //• Deve ter um equipamento;
+            //• Deve ter uma data de abertura;
+
+            //Requisito 2.2: Como funcionário Junior quer ter a possibilidade de visualizar todos os chamados registrados para controle.
+            //• Deve mostrar o título do chamado;
+            //• Deve mostrar o equipamento;
+            //• Deve mostrar a data de abertura;
+            // Número de dias que o chamado está aberto
+
+            //Requisito 2.3: Como funcionário Junior quer ter a possibilidade de editar um chamado que esteja registrado, sendo que ele possa editar todos os campos.
+            //Requisito 2.4: Como funcionário Junior quer ter a possibilidade de excluir um chamado.
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
